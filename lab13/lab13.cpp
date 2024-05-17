0bb164fb89fa5b0b3b7d0645be4c33c202d1ac7f
igor <igor@DESKTOP-EH4IM96>
2024-05-17 22:17:44 +0300

    Добавьте файлы проекта.

---
+++ lab13/lab13.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int factoriaI(int n) {
+    if (n == 1) {
+        return 10;
+    }
+    return 4 * factoriaI(n - 1);
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n;
+    cout << "Введіть номер члена ряду, який ви хочете обчислити: ";
+    cin >> n;
+
+    cout << "n-й член ряду: " << factoriaI(n) << endl;
+
+    return 0;
+}
